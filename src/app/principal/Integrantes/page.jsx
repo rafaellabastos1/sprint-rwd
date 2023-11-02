a3ca258d688149a0b457b1d8a203a6ef9617a48e
rafaellabastos <rafa.bastos@yahoo.com.br>
2023-11-02 17:26:10 -0300

    página dos integrantes criada

---
+++ src/app/principal/Integrantes/page.jsx
@@ -0,0 +1,15 @@
+export default function Integrantes() {
+    return(
+        <>
+            <h2>Integrantes</h2>
+
+            <ul>
+                <li>Douglas Magalhães de Araújo - RM: 552008</li>
+                <li>Gustavo Argüello Bertacci - RM: 551304</li>
+                <li>Igor Ribeiro Anccilotto - RM: 550415 </li>
+                <li>Luiz Fillipe Farias - RM: 99519  </li>
+                <li>Rafaella Monique do Carmo - RM: 552425</li>
+            </ul>
+        </>
+    )
+}
