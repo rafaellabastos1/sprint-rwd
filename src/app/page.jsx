ae6da3cfb28bed1fd138a95834bb436e28c8e794
rafaellabastos <rafa.bastos@yahoo.com.br>
2023-11-02 17:21:33 -0300

    fiz a página home e otimizei os textos

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,7 +1,20 @@
 export default function Home() {
   return (
     <>
+      <p>A Porto Seguro é uma empresa fundada em 1945 e que se especializa na área de seguros, soluções financeiras e serviços em geral.</p>
+
+      <p>A TechnoBike, em parceria com a FIAP, pretende automatizar o processo de vistoria de Bikes com sua realização feita on-line, com a utilização de Inteligência Artificial e sem a necessidade de intervenção humana no processo. Dessa forma, será promovido a você uma maior rapidez e comodidade</p>
     
+      <h2>Faça sua vistoria aqui:</h2>
+      <ul>
+        <li><Link href=''>Vistoria</Link></li>
+        <ul>
+          <li>Identificação do cliente</li>
+          <li>Tipo do Seguro</li>
+          <li>Vistoria</li>
+          <li>Status da vistoria</li>
+        </ul>
+      </ul>
     </>
   )
 }
