5a53dab621df83ee8d61868b963e3216c7f9a9d6
rafaellabastos <rafa.bastos@yahoo.com.br>
2023-11-02 17:33:32 -0300

    otimização da página de inicio da vistoria

---
+++ src/app/principal/Vistoria/page.jsx
@@ -0,0 +1,16 @@
+import Link from 'next/link'
+
+export default function Vistoria() {
+    return (
+        <>
+            <h2>Processo de vistoria</h2>
+
+            <p>Olá, seja bem vindo à TechnoBike.</p>
+
+            <p>Primeira etapa: </p>
+            <ul>
+                <li><Link href=''>Identificação</Link></li>
+            </ul>
+        </>
+    )
+}
