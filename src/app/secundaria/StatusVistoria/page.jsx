3eb42f858f1273b1f99c3b898221f83305b5d6fe
rafaellabastos <rafa.bastos@yahoo.com.br>
2023-11-02 17:41:40 -0300

    página de status da vistoria com texto reformulado

---
+++ src/app/secundaria/StatusVistoria/page.jsx
@@ -0,0 +1,18 @@
+export default function StatusVistoria() {
+
+    const conferir = () =>{
+        alert("Sua vistoria está em análise!")
+    }
+
+    return (
+        <>
+            <h1>Status da Vistoria</h1>
+            <div>
+                <p>Você ficará sabendo do andamento de sua vistoria pelo seu e-mail. Por lá, você saberá se está em análise, com documentos faltando, se foi aprovado ou reprovado.</p>
+
+                <p>Também é possível acompanhar o status da sua vistoria por aqui:</p>
+                <button type="button" onClick={conferir}>Status da vistoria</button>
+            </div>
+        </>
+    )
+}
