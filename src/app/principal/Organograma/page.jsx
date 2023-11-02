a8013e171c018741db30391f9e44520571a229ea
rafaellabastos <rafa.bastos@yahoo.com.br>
2023-11-02 17:30:43 -0300

    escopo da página organograma criado, falta linkar as imagens

---
+++ src/app/principal/Organograma/page.jsx
@@ -0,0 +1,29 @@
+import Link from 'next/link'
+
+export default function Organograma() {
+    return (
+        <>
+            <h2>Organograma</h2>
+            
+            <p>Segue abaixo o organograma das páginas:</p>
+            
+            <p>Início da vistoria</p>
+            <Link href=''></Link>
+            
+            <p>Identificação do cliente</p>
+            <Link href=''></Link>
+            
+            <p>Escolha do tipo do seguro</p>
+            <Link href=''></Link>
+            
+            <p>Envio dos arquivos da vistoria</p>
+            <Link href=''></Link>
+            
+            <p>Status da Vistoria</p>
+            <Link href=''></Link>
+            
+            <p>Integrantes</p>
+            <Link href=''></Link>
+        </>
+    )
+}
