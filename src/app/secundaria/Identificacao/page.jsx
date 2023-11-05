9c7a70be6361d324e9a2bd52c4557cae4d41cb16
rafaellabastos <rafa.bastos@yahoo.com.br>
2023-11-05 18:32:48 -0300

    comentei a parte do client component

--- src/app/secundaria/Identificacao/page.jsx
+++ src/app/secundaria/Identificacao/page.jsx
@@ -1,34 +1,33 @@
 import Link from 'next/link'
 import Page from '../TipoSeguro/page';
-import { useState} from 'react';
-import { useClient} from 'next/client';
+// import { useState} from 'react';
+// import { useClient} from 'next/client';
 
 export default function Identificacao() {
-    const [cpf, setCpf] = useState("");
-    const [aviso, setAviso] = useState("");
-    useClient();
+    // const [cpf, setCpf] = useState("");
+    // const [aviso, setAviso] = useState("");
+    // useClient();
 
-    const enviarCpf = () => {
-        setAviso("CPF enviado com sucesso. Você pode continuar agora!");
-    }
+    // const enviarCpf = () => {
+    //     setAviso("CPF enviado com sucesso. Você pode continuar agora!");
+    // }
 
     return (
         <>
             <h1>Identificação</h1>
 
             <p>Informe seu CPF:</p>
-            <input type="text" value={cpf} onChange={(e) => setCpf(e.target.value)} id="ColetarCpf" placeholder="Digite aqui"/>
-            <button id="EnviarCPF" onClick={enviarCpf}>Enviar</button> 
+            {/* <input type="text" value={cpf} onChange={(e) => setCpf(e.target.value)} id="ColetarCpf" placeholder="Digite aqui"/>
+            <button id="EnviarCPF" onClick={enviarCpf}>Enviar</button>  */}
+            <input type="text" id="ColetarCpf" placeholder="Digite aqui"/>
+            <button id="EnviarCpf">Enviar</button>
 
-            {aviso && (
-                <div>
-                    <p>{aviso}</p>
-                    <p>Próxima etapa: </p>
-                    <ul>
-                        <li><Link href='/secundaria/TipoSeguro'>Escolha do tipo do seguro</Link></li>
-                    </ul>
-                </div>
-            )}  
+            <div>
+                <p>Próxima etapa: </p>
+                <ul>
+                    <li><Link href='/secundaria/TipoSeguro'>Escolha do tipo do seguro</Link></li>
+                </ul>
+            </div>
         </>
     )
 }
