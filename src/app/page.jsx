313a60e0a9fdc4839f3f68cec08c2af0706ae3b8
rafaellabastos1 <rm552425@fiap.com.br>
2023-11-13 10:31:12 -0300

    adicionei as imagens e os links com seus respectivos divs e className

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,22 +1,42 @@
 import Link from 'next/link'
+import Image from 'next/image'
 
 export default function Home() {
   return (
     <>
-      <p>A Porto Seguro é uma empresa fundada em 1945 e que se especializa na área de seguros, soluções financeiras e serviços em geral.</p>
+      <div className='textoHome'>
+        <p>A Porto Seguro é uma empresa fundada em 1945 e que se especializa na área de seguros, soluções financeiras e serviços em geral.</p>
 
-      <p>A TechnoBike, em parceria com a FIAP, pretende automatizar o processo de vistoria de Bikes com sua realização feita on-line, com a utilização de Inteligência Artificial e sem a necessidade de intervenção humana no processo. Dessa forma, será promovido a você uma maior rapidez e comodidade</p>
-    
-      <h2>Faça sua vistoria aqui:</h2>
-      <ul>
-        <li><Link href='/principal/vistoria'>Vistoria</Link></li>
-        <ul>
-          <li>Identificação do cliente</li>
-          <li>Tipo do Seguro</li>
-          <li>Vistoria</li>
-          <li>Status da vistoria</li>
-        </ul>
-      </ul>
+        <p>A TechnoBike, em parceria com a FIAP, pretende automatizar o processo de vistoria de Bikes com sua realização feita on-line, com a utilização de Inteligência Artificial e sem a necessidade de intervenção humana no processo. Dessa forma, será promovido a você uma maior rapidez e comodidade.</p>
+      </div>
+
+      <div className='grupoBike'>
+        <figure className='fotoBike'>
+          <Image src='/images/bike.jpeg'
+            width={514}
+            height={239}
+            alt="Bicicleta">
+          </Image>
+        </figure>
+
+        <div className='cardBike'>
+          <Link href='/principal/vistoria'>Faça sua vistoria aqui</Link>
+        </div>
+      </div>
+
+      <div className='grupoIntegrantes'>
+        <div className='cardIntegrantes'>
+          <Link href='/principal/integrantes'>Conheça nossos integrantes</Link>
+        </div>
+
+        <figure className='fotoMao'>
+          <Image src='/images/mao.jpeg'
+            width={468}
+            height={239}
+            alt="Mão segurando um cadeado">
+          </Image>
+        </figure>
+      </div>
     </>
   )
 }
