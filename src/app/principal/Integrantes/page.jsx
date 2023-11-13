76b8f290f0d3281a7b760b78ac4efba5954ebc53
rafaellabastos1 <rm552425@fiap.com.br>
2023-11-13 10:32:13 -0300

    não

--- src/app/principal/Integrantes/page.jsx
+++ src/app/principal/Integrantes/page.jsx
@@ -3,13 +3,13 @@ export default function Integrantes() {
         <>
             <h2>Integrantes</h2>
 
-            <ul>
-                <li>Douglas Magalhães de Araújo - RM: 552008</li>
-                <li>Gustavo Argüello Bertacci - RM: 551304</li>
-                <li>Igor Ribeiro Anccilotto - RM: 550415 </li>
-                <li>Luiz Fillipe Farias - RM: 99519  </li>
-                <li>Rafaella Monique do Carmo - RM: 552425</li>
-            </ul>
+                <ul>
+                    <li>Douglas Magalhães de Araújo - RM: 552008</li>
+                    <li>Gustavo Argüello Bertacci - RM: 551304</li>
+                    <li>Igor Ribeiro Anccilotto - RM: 550415 </li>
+                    <li>Luiz Fillipe Farias - RM: 99519  </li>
+                    <li>Rafaella Monique do Carmo - RM: 552425</li>
+                </ul>
 
             <p><b>Link do GitHub: </b>https://github.com/rafaellabastos1/sprint-rwd.git</p>
             <p><b>Link da Vercel: </b>https://vercel.com/rm552425-fiapcombr/sprint-rwd/A9jHkeom2VGu7Z5HmyqVcHH4CTWv</p>
