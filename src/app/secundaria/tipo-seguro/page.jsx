9602563e60fe51343ec911e3c29bbf5ab8d95903
rafaellabastos <rafa.bastos@yahoo.com.br>
2023-11-15 20:16:14 -0300

    exclui o identificar cpf

--- src/app/secundaria/tipo-seguro/page.jsx
+++ src/app/secundaria/tipo-seguro/page.jsx
@@ -2,79 +2,13 @@
 import { useState, useEffect } from 'react';
 import Link from 'next/link';
 
-
-// Componente funcional TipoSeguro
 export default function TipoSeguro() {
+
     // Declara e inicializa variáveis de estado usando o hook useState
     const [aviso, setAviso] = useState("");
-    const [cpfNovo, setCpfNovo] = useState("")
-    const [clientesCadastrados, setclientesCadastrados] = useState([])
-    const [clienteNovo, setClienteNovo] = useState({
-    cpf : "",
-    opcSeguro: "",
-    bikeInteira: "Sim",
-    numSerie: "987654",
-    roda: "redonda",
-    freios: "Disco",
-    guidao: "Curvo",
-    pedais: "Clip",
-    corrente: "Dupla",
-    clienteBike: "Cliente42",
-    bikeFrente: "sim",
-    acessorios: "Farol",
-    videoBike: "https://www.youtube.com/watch?v=abc12345678",
-    videoPartes: "https://www.youtube.com/watch?v=def98765432",
-    analiseVistoria : "Em análise"
-})
-useEffect(() => {
-    fetch(`http://localhost:8080/technobike/${cpfNovo}`, {
-        method: "get"
-    }).then((resp) =>{
-        return resp.json();
-    }).then((resp) =>{
-        setclientesCadastrados(resp)
-        console.log(resp)
-        tipoSeguro(0)
-    })
-    .catch((error) => {
-        console.log(error)
-    })
-}, [cpfNovo])  
-
-const handleChange = (tipoSeguro) =>{
-    setClienteNovo((prevClienteNovo) => ({
-        ...prevClienteNovo,
-        cpf: cpfNovo,
-        opcSeguro: tipoSeguro
-    }))
-    handleSubmit()
-}
-const handleSubmit = async () =>{
-    
-    console.log(clienteNovo)
-    try {
-        const response = await fetch('http://localhost:8080/technobike/', {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify(clienteNovo),
-        });
-  
-        if (response.ok) {
-          console.log("Inserção bem-sucedida!");
-          setAviso("Sucesso ao enviar!");
-        } else {
-          console.error("Erro ao inserir: ", response.status, response.statusText);
-        }
-      } catch (error) {
-        console.error("Erro durante a solicitação: ", error.message);
-      }
-    };
 
     // Função para definir o tipo de seguro com base no valor recebido como parâmetro
     const tipoSeguro = (seguro) => {
-       
         if (seguro === 0) {
             setAviso("Seguro para ciclistas que pedalam na rua selecionado!");
         } else if (seguro === 1) {
@@ -90,25 +24,12 @@ const handleSubmit = async () =>{
         } else if (seguro === 6) {
             setAviso("Seguro para ciclistas que viajam com a bike selecionado!");
         } 
-        setClienteNovo((prevClienteNovo) => ({
-            ...prevClienteNovo,
-            cpf : cpfNovo,
-            opcSeguro: toString()
-        }))
-        handleChange(seguro)
     }
 
-    // Código JSX representando a interface do componente
     return (
         <>
             {/* Cabeçalho */}
-            <h1 className='subtitulo'>&nbsp;&nbsp;TechnoBike</h1>
-            
-            <p className='textoIdentificacao'>&nbsp;&nbsp;Informe seu CPF:</p>
-            <input type="text" value={cpfNovo} onChange={(e) => setCpfNovo(e.target.value)} id="ColetarCpf" placeholder="Digite aqui"/>
-            
-            {/* Botão para enviar o CPF */}
-            <button id="EnviarCPF">Enviar</button> 
+            <h1 className='subtitulo'>TechnoBike</h1>
 
             {/* Título principal */}
             <h2>Escolha do tipo do seguro</h2>
